940dfed8839a647aedd8baa9ff72ae8f3c539b9d
Salma <ifksalma@gmail.com>
2023-11-12 20:35:11 +0100

    TP stat

---
+++ LightOff_IFAKIREN_version_graphique/src/lightoff_ifakiren_version_console/PanneauBoutonsVerticaux.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_ifakiren_version_console;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author ifksa
+ */
+class PanneauBoutonsVerticaux {
+
+    static void setLayout(GridLayout gridLayout) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void add(JButton bouton_ligne) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ LightOff_IFAKIREN_version_graphique/src/lightoff_ifakiren_version_console/Partie.java
@@ -0,0 +1,79 @@
+package lightoff_ifakiren_version_console;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ifksa
+ */
+import java.util.Scanner;
+
+public class Partie {
+
+    private GrilleDeJeu grille;
+    private int nbCoups;
+
+    public Partie() {
+        this.grille = new GrilleDeJeu(7, 7); // Pour simplifier, une grille 7x7 est créée par défaut
+        this.nbCoups = 0;
+    }
+
+    public void initialiserPartie() {
+        this.grille.melangerMatriceAleatoirement(10); // Pour simplifier, nous mélangeons 10 fois par défaut
+    }
+
+    public void lancerPartie() {
+        initialiserPartie();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bienvenue dans le jeu LightOff!");
+        System.out.println(grille);
+
+        while (!grille.cellulesToutesEteintes()) {
+            System.out.print("Entrez votre coup (format : l5, c3, d1, d2): ");
+            String coup = scanner.nextLine();
+
+            if (coup.length() >= 2) { // Vérifier la longueur de la chaîne de coup
+                jouerCoup(coup);
+            } else {
+                System.out.println("Coup non valide! Veuillez réessayer.");
+            }
+
+            System.out.println("Coups joués: " + nbCoups);
+            System.out.println(grille);
+        }
+
+        System.out.println("Bravo! Vous avez terminé le jeu en " + nbCoups + " coups.");
+        scanner.close();
+    }
+
+    private void jouerCoup(String coup) {
+        switch (coup.charAt(0)) {
+            case 'l':
+                grille.activerLigneDeCellules(Integer.parseInt(coup.substring(1)));
+                break;
+            case 'c':
+                grille.activerColonneDeCellules(Integer.parseInt(coup.substring(1)));
+                break;
+            case 'd':
+                if (coup.charAt(1) == '1') {
+                    grille.activerDiagonaleDescendante();
+                } else {
+                    grille.activerDiagonaleMontante();
+                }
+                break;
+            default:
+                System.out.println("Coup non valide! Veuillez réessayer.");
+                return;
+        }
+        nbCoups++;
+    }
+
+    public static void main(String[] args) {
+        Partie partie = new Partie();
+        partie.lancerPartie();
+    }
+}
